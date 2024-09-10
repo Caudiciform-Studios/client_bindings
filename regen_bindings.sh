4da4b53c2d2fd46d28bea1ec15043bbf6df51576
Alec Deason <alec@tinycountry.com>
2024-09-10 12:59:23 -0700

    get python working

---
+++ regen_bindings.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+#set -e
+
+cargo component build
+./fix.sh
+#cargo component build
+#./fix.sh
--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,109 @@
-use core::cmp::{Ord, Ordering};
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+pub use bindings::*;
 
-mod bindings;
-pub use bindings::game::auto_rogue::types::{
-    ActionTarget, AttackParams, Direction, EquipmentSlot, MicroAction,
-};
-pub use bindings::{
-    actions, config_data, creature_at, events, export, get_equipment_state, inventory, item_at,
-    visible_tiles, Guest, *,
-};
+mod bindings {
+    use core::cmp::{Ord, Ordering};
+    use std::ops::{Add, AddAssign, Sub, SubAssign};
 
-impl Default for Loc {
-    fn default() -> Loc {
-        Loc { x: 0, y: 0 }
-    }
-}
+    wit_bindgen::generate!({
+        world: "auto-rogue-ai",
+        additional_derives: [PartialEq, Eq, Hash, Clone, serde::Serialize, serde::Deserialize],
+        pub_export_macro: true,
+    });
 
-impl Add for Loc {
-    type Output = Self;
+    pub use crate::game::auto_rogue::types::*;
 
-    fn add(self, other: Self) -> Self {
-        Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
+
+    impl Default for Loc {
+        fn default() -> Loc {
+            Loc { x: 0, y: 0 }
         }
     }
-}
-impl Sub for Loc {
-    type Output = Self;
 
-    fn sub(self, other: Self) -> Self {
-        Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
+    impl Add for Loc {
+        type Output = Self;
+
+        fn add(self, other: Self) -> Self {
+            Self {
+                x: self.x + other.x,
+                y: self.y + other.y,
+            }
         }
     }
-}
-impl AddAssign for Loc {
-    fn add_assign(&mut self, other: Self) {
-        *self = Self {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        };
-    }
-}
-impl SubAssign for Loc {
-    fn sub_assign(&mut self, other: Self) {
-        *self = Self {
-            x: self.x - other.x,
-            y: self.y - other.y,
-        };
-    }
-}
+    impl Sub for Loc {
+        type Output = Self;
 
-impl Loc {
-    pub fn magnitude(&self) -> f32 {
-        ((self.x.pow(2) + self.y.pow(2)) as f32).sqrt()
-    }
-}
-impl Ord for Loc {
-    fn cmp(&self, other: &Self) -> Ordering {
-        (self.x, self.y).cmp(&(other.x, other.y))
-    }
-}
-
-impl PartialOrd for Loc {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl From<Direction> for String {
-    fn from(slot: Direction) -> String {
-        match slot {
-            Direction::North => "North".to_string(),
-            Direction::NorthEast => "NorthEast".to_string(),
-            Direction::East => "East".to_string(),
-            Direction::SouthEast => "SouthEast".to_string(),
-            Direction::South => "South".to_string(),
-            Direction::SouthWest => "SouthWest".to_string(),
-            Direction::West => "West".to_string(),
-            Direction::NorthWest => "NorthWest".to_string(),
+        fn sub(self, other: Self) -> Self {
+            Self {
+                x: self.x - other.x,
+                y: self.y - other.y,
+            }
         }
     }
-}
-
-impl From<EquipmentSlot> for String {
-    fn from(slot: EquipmentSlot) -> String {
-        match slot {
-            EquipmentSlot::LeftHand => "LeftHand".to_string(),
-            EquipmentSlot::RightHand => "RightHand".to_string(),
+    impl AddAssign for Loc {
+        fn add_assign(&mut self, other: Self) {
+            *self = Self {
+                x: self.x + other.x,
+                y: self.y + other.y,
+            };
         }
     }
-}
+    impl SubAssign for Loc {
+        fn sub_assign(&mut self, other: Self) {
+            *self = Self {
+                x: self.x - other.x,
+                y: self.y - other.y,
+            };
+        }
+    }
 
-impl std::convert::TryFrom<String> for EquipmentSlot {
-    type Error = anyhow::Error;
-    fn try_from(slot: String) -> anyhow::Result<EquipmentSlot> {
-        Ok(match slot.as_str() {
-            "LeftHand" => EquipmentSlot::LeftHand,
-            "RightHand" => EquipmentSlot::RightHand,
-            _ => anyhow::bail!("Unknown slot: {slot}"),
-        })
+    impl Loc {
+        pub fn magnitude(&self) -> f32 {
+            ((self.x.pow(2) + self.y.pow(2)) as f32).sqrt()
+        }
+    }
+    impl Ord for Loc {
+        fn cmp(&self, other: &Self) -> Ordering {
+            (self.x, self.y).cmp(&(other.x, other.y))
+        }
+    }
+
+    impl PartialOrd for Loc {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+    impl From<Direction> for String {
+        fn from(slot: Direction) -> String {
+            match slot {
+                Direction::North => "North".to_string(),
+                Direction::NorthEast => "NorthEast".to_string(),
+                Direction::East => "East".to_string(),
+                Direction::SouthEast => "SouthEast".to_string(),
+                Direction::South => "South".to_string(),
+                Direction::SouthWest => "SouthWest".to_string(),
+                Direction::West => "West".to_string(),
+                Direction::NorthWest => "NorthWest".to_string(),
+            }
+        }
+    }
+
+    impl From<EquipmentSlot> for String {
+        fn from(slot: EquipmentSlot) -> String {
+            match slot {
+                EquipmentSlot::LeftHand => "LeftHand".to_string(),
+                EquipmentSlot::RightHand => "RightHand".to_string(),
+            }
+        }
+    }
+
+    impl std::convert::TryFrom<String> for EquipmentSlot {
+        type Error = anyhow::Error;
+        fn try_from(slot: String) -> anyhow::Result<EquipmentSlot> {
+            Ok(match slot.as_str() {
+                "LeftHand" => EquipmentSlot::LeftHand,
+                "RightHand" => EquipmentSlot::RightHand,
+                _ => anyhow::bail!("Unknown slot: {slot}"),
+            })
+        }
     }
 }
